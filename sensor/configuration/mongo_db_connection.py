cc46b84d7673d2a68e24f1c9d19005490e5532e4
Umang2002 <uadobariya402@gmail.com>
2023-07-07 11:56:04 +0530

    MongoDB Connected

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,24 @@
+import pymongo
+from sensor.constant.database import DATABASE_NAME
+# from sensor.constant.env_variable import MONGODB_URL_KEY
+import certifi
+import os
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://Umang1436:Umang1436@sensor-fault-detection.n5mlwv4.mongodb.net/"
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
