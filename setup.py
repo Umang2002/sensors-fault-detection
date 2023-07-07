0111f68ad142d548c7105fe737d73b7885cbe67a
Umang2002 <uadobariya402@gmail.com>
2023-07-07 10:41:05 +0530

    Project setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+    return requirement_list
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Umang",
+    author_email="uadobariya402@gmail.com",
+    packages = find_packages(),
+    install_requires=["pymongo==4.2.0"]
+)
