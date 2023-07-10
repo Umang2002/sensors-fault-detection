a4d7409ea292907a698f98b6a735e3f4034fcbf4
Umang2002 <uadobariya402@gmail.com>
2023-07-10 17:07:01 +0530

    Data Ingestion part Added

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -2,6 +2,8 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
+from sklearn.model_selection import train_test_split
+from sensor.data_access.sensor_data import SensorData
 import os,sys
 from pandas import DataFrame
 
@@ -13,22 +15,70 @@ class DataIngestion:
         except Exception as e:
             raise SensorException(e,sys)
 
-    def export_data_into_featutre_stor(self) -> DataFrame:
+    def export_data_into_featutre_store(self) -> DataFrame:
         """
         Export mongodb collection record as data frame into feature
         """
+        try:
+            logging.info("Exporting data from mongodb to featureStore")
+            sensor_data = SensorData()
+            dataframe = sensor_data.export_collection_as_dataframe(collection_name=self.data_ingestion_config.collection_name)
+
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+
+            # creating folder
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
+            # save the data
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
+
+        except Exception as e:
+            raise SensorException(e,sys)
         pass
 
     def split_data_as_train_test(self,dataframe:DataFrame) -> None:
         """
         Feature store dataset will be divided into train and test split
         """
+        try:
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
+
+            logging.info("Performed train test split on the dataframe")
+
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+
+            os.makedirs(dir_path, exist_ok=True)
+
+            logging.info(f"Exporting train and test file path.")
+
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
+
+            logging.info(f"Exported train and test file path.")
+        except Exception as e:
+            raise SensorData(e,sys)
         
         pass
 
    
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
          try:
-             pass
+             dataframe = self.export_data_into_featutre_store()
+             self.split_data_as_train_test(dataframe=dataframe)
+             data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,test_file_path=self.data_ingestion_config.testing_file_path)
+             return data_ingestion_artifact
          except Exception as e:
              raise SensorException(e,sys)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -5,19 +5,23 @@ from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.exception import SensorException
 import os,sys
 from sensor.logger import logging
-
+from sensor.components.data_ingestion import DataIngestion
 class TrainPipeline():
 
     def __init__(self):
-        training_pipeline_config = TrainingPipelineConfig()
-        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        self.training_pipeline_config = training_pipeline_config
+        self.training_pipeline_config = TrainingPipelineConfig()
+        # self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        # self.training_pipeline_config = training_pipeline_config
 
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Starting data ingestion")
-            logging.info("Data ingestion Completed")
 
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact= data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion Completed and artifact:{data_ingestion_artifact}")
+            return data_ingestion_artifact
         except Exception as e:
             raise SensorException(e,sys)
         
@@ -54,6 +58,7 @@ class TrainPipeline():
     def run_pipeline(self):
         try:
            data_ingestion_artifact:DataIngestionArtifact= self.start_data_ingestion()
+           return data_ingestion_artifact
         except Exception as e:
             raise SensorException(e,sys) 
 
